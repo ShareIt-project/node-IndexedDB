fad2c87947e04a4cddb010b9632e47faf03830c7
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-11-19 01:55:43 +0100

    Removed checker and install function from polyfill (they are useless)

--- IndexedDB.js
+++ IndexedDB.js
@@ -1,195 +1,161 @@
-// Polyfill to add full support for JavaScript objects with an IndexedDB
-// interface. This is primary purpose because a bug storing files on Chromium
-// implementation (http://code.google.com/p/chromium/issues/detail?id=108012).
-// The drawback is that since it's implemented using pure JavaScript objects,
-// well... the data persistence lacks about some functionality... :-P
+// IndexedDB module for Node.js
+//
+// Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
+//
+// This module implements the IndexedDB specification using the LevelDB module
+// as underlying database engine. It's mainly intention is to port client-side
+// IndexedDB intensive applications to Node.js.
+//
+// It is based on IndexedDB-javascript.js polyfill from ShareIt! project
+// https://github.com/piranna/ShareIt
 
-window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
 
-
-function testIDBBlobSupport(callback)
+function IDBRequest()
 {
-  var dbname = "detect-blob-support";
-  var idb = indexedDB;
-
-  idb.deleteDatabase(dbname).onsuccess = function()
-  {
-    var request = idb.open(dbname, 1);
-    request.onupgradeneeded = function()
-    {
-      request.result.createObjectStore("store");
-    };
-    request.onsuccess = function()
-    {
-      var db = request.result;
-      try
-      {
-        db.transaction("store", "readwrite").objectStore("store").put(new Blob(), "key");
-        callback(true);
-      }
-      catch(e)
-      {
-        callback(false);
-      }
-      finally
-      {
-        db.close();
-        idb.deleteDatabase(dbname);
-      }
-    };
-  };
+  this.target = {};
 }
-
-
-function IdbJS_install()
+IDBRequest.prototype =
 {
-	function IDBRequest()
-	{
-	  this.target = {}
-	}
-	IDBRequest.prototype =
-	{
-	  set onsuccess(func)
-	  {
-	    this._onsuccess = func
-	    var event = {target: this.target}
-	    func.call(this, event)
-	  }
-	}
+  set onsuccess(func)
+  {
+    this._onsuccess = func;
+    var event = {target: this.target};
+    func.call(this, event);
+  }
+};
 
-	function IDBOpenRequest()
-	{
-	  IDBRequest.call(this)
+function IDBOpenRequest()
+{
+  IDBRequest.call(this);
 
 //      this.prototype.__defineSetter__("onupgradeneeded", function(func)
 //      {
 //        var event = {target: this.target}
 //        func.call(this, event)
 //      })
-	}
-	IDBOpenRequest.prototype = new IDBRequest()
+}
+IDBOpenRequest.prototype = new IDBRequest();
 
 
-	function IDBCursor()
-	{
-	  this._objects = []
-	  this._index = 0
+function IDBCursor()
+{
+  this._objects = [];
+  this._index = 0;
 
-	  this.continue = function()
-	  {
-	    this._index += 1
+  this.continue = function()
+  {
+    this._index += 1;
 
-        var event = {target: {}}
-        if(this.value)
-            event.target.result = this
-	    this._request._onsuccess(event)
-	  }
-	}
-    IDBCursor.prototype =
+    var event = {target: {}};
+    if(this.value)
+        event.target.result = this;
+    this._request._onsuccess(event);
+  };
+}
+IDBCursor.prototype =
+{
+  get value()
+  {
+    return this._objects[this._index];
+  }
+};
+
+function IDBObjectStore()
+{
+  var objects = {}
+
+  this.add = function(value, key)
+  {
+    return this.put(value, key);
+  }
+  this.get = function(key)
+  {
+    var request = new IDBRequest();
+        request.result = objects[key];
+    return request;
+  }
+  this.openCursor = function(range)
+  {
+    var request = new IDBRequest();
+
+    if(Object.keys(objects).length)
     {
-      get value()
-      {
-        return this._objects[this._index]
-      }
+        // Fill the cursor with the objectstore objects
+        var cursor = new IDBCursor();
+        for(var key in objects)
+            cursor._objects.push(objects[key]);
+
+        // Link the request and the cursor between them
+        request.target.result = cursor;
+        cursor._request = request;
     }
 
-	function IDBObjectStore()
-	{
-	  var objects = {}
+    return request;
+  }
+  this.put = function(value, key)
+  {
+    if(this.keyPath)
+    {
+       if(key)
+           throw DOMException;
+       key = value[this.keyPath];
+    }
 
-	  this.add = function(value, key)
-	  {
-	    return this.put(value, key)
-	  }
-	  this.get = function(key)
-	  {
-	    var request = new IDBRequest()
-	        request.result = objects[key]
-	    return request
-	  }
-	  this.openCursor = function(range)
-	  {
-        var request = new IDBRequest()
+   if(!key)
+       throw DOMException;
 
-        if(Object.keys(objects).length)
-        {
-            // Fill the cursor with the objectstore objects
-            var cursor = new IDBCursor()
-            for(var key in objects)
-                cursor._objects.push(objects[key])
+    objects[key] = value;
 
-            // Link the request and the cursor between them
-            request.target.result = cursor
-            cursor._request = request
-        }
-
-	    return request
-	  }
-	  this.put = function(value, key)
-	  {
-	    if(this.keyPath)
-	    {
-	       if(key)
-	           throw DOMException
-	       key = value[this.keyPath]
-	    }
-
-       if(!key)
-           throw DOMException
-
-	    objects[key] = value
-
-	    var request = new IDBRequest()
-	        request.result = objects[key]
-	    return request
-	  }
-	}
-
-	function IDBTransaction()
-	{
-	  this.objectStore = function(name)
-	  {
-	    return this.db._stores[name]
-	  }
-	}
-
-	function IDBDatabase()
-	{
-	  this._stores = {}
-
-      this.createObjectStore = function(name, optionalParameters)
-      {
-        var objectStore = new IDBObjectStore()
-        if(optionalParameters)
-            objectStore.keyPath = optionalParameters.keyPath
-
-        this._stores[name] = objectStore
-      }
-
-      this.setVersion = function(version)
-      {
-        this.version = version
-
-        return new IDBRequest()
-      }
-
-	  this.transaction = function(storeNames, mode)
-	  {
-	    var result = new IDBTransaction()
-	        result.db = this
-
-	    return result
-	  }
-	}
-
-
-    window.indexedDB._dbs = {}
-    window.indexedDB.open = function(name, version)
-	{
-	  this._dbs[name] = this._dbs[name] || new IDBDatabase()
-
-	  var request = new IDBOpenRequest()
-	      request.result = this._dbs[name]
-	  return request
-	}
+    var request = new IDBRequest();
+        request.result = objects[key];
+    return request;
+  };
 }
+
+function IDBTransaction()
+{
+  this.objectStore = function(name)
+  {
+    return this.db._stores[name];
+  };
+}
+
+function IDBDatabase()
+{
+  this._stores = {};
+
+  this.createObjectStore = function(name, optionalParameters)
+  {
+    var objectStore = new IDBObjectStore();
+    if(optionalParameters)
+        objectStore.keyPath = optionalParameters.keyPath;
+
+    this._stores[name] = objectStore;
+  }
+
+  this.setVersion = function(version)
+  {
+    this.version = version;
+
+    return new IDBRequest();
+  }
+
+  this.transaction = function(storeNames, mode)
+  {
+    var result = new IDBTransaction();
+        result.db = this;
+
+    return result;
+  };
+}
+
+
+window.indexedDB._dbs = {};
+window.indexedDB.open = function(name, version)
+{
+  this._dbs[name] = this._dbs[name] || new IDBDatabase();
+
+  var request = new IDBOpenRequest();
+      request.result = this._dbs[name];
+  return request;
+};
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Jesús Leganés Combarro "Piranna" - [piranna@gmail.com]
 This module implements the IndexedDB specification using the LevelDB module as
 underlying database engine. It's mainly intention is to port client-side
 IndexedDB intensive applications to Node.js.
+
+It is based on IndexedDB-javascript.js polyfill from ShareIt! project
+https://github.com/piranna/ShareIt
